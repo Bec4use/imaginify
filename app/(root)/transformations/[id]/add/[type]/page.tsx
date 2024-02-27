48b5a0453e5b3c3ec8db55b8abfe8445337691cc
Bec4use <netipong.clk@gmail.com>
2024-02-28 05:47:00 +0700

    Add Imaginify pages

--- app/(root)/transformations/[id]/add/[type]/page.tsx
+++ app/(root)/transformations/[id]/add/[type]/page.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+// import Header from '@/components/shared/Header'
+// import React from 'react'
 
-const AddTransformationTypePage = () => {
-  return (
-    <div>AddTransformationTypePage</div>
-  )
-}
+// const AddTransformationTypePage = () => {
+//   return (
+//    <Header 
+//     title="Transformation Title"
+//     subtitle="Transformation Subtitle"
+//    />
+//   )
+// }
 
-export default AddTransformationTypePage
+// export default AddTransformationTypePage
